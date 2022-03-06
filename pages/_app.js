f314684d9969569b23de15838c4c972abdd471d0
Ankur Singh <abishek.singh.chauhan@gmail.com>
2022-03-06 21:52:55 +0545

    supabase database and auth implemented.

---
+++ pages/_app.js
@@ -0,0 +1,32 @@
+// import type { AppProps } from 'next/app';
+import Layout from '../components/Layout/Layout';
+import Chakra from '../components/Chakra';
+import { useState, useEffect } from 'react';
+import { supabase } from '../lib/supabaseClient';
+import Auth from '../components/Auth';
+import Account from '../components/Account';
+
+function MyApp({ Component, pageProps }) {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    setSession(supabase.auth.session());
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+  return (
+    <Chakra cookies={pageProps.cookies}>
+      {!session ? (
+        <Auth />
+      ) : (
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      )}
+    </Chakra>
+  );
+}
+
+export default MyApp;

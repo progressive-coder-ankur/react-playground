0f005a3044e5203189c8b177aadcd8b81222bbe1
Ankur Singh <abishek.singh.chauhan@gmail.com>
2022-03-06 19:10:41 +0545

    First commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import Layout from '../components/Layout/Layout';
+import Chakra from '../components/Chakra';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Chakra cookies={pageProps.cookies}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Chakra>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ styles/theme.js
@@ -0,0 +1,20 @@
+// 1. Import the extendTheme function
+import { extendTheme } from '@chakra-ui/react';
+
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+};
+
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+};
+
+const theme = extendTheme({ colors, config });
+
+export default theme;
